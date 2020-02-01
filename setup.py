8d40174be3ebf938316514131410c7d4b11f730e
Anurag-Aggarwal <44736106+Anurag-Aggarwal@users.noreply.github.com>
2020-02-01 16:33:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Anurag Aggarwal',                   # Type in your name
   author_email = 'agrawalanurag321@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/101703088-topsis',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Anurag-Aggarwal/101703088-topsis/archive/v_2.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Anurag-Aggarwal/101703088-topsis/archive/v_2.0.2.tar.gz',    # I explain this later on
   #keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           "pandas","numpy"
